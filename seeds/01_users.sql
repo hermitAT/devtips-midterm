55a04f6b83e76c665c672f8ae0359768ae0144a3
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-28 05:23:53 +0000

    Add users seed file

---
+++ seeds/01_users.sql
@@ -0,0 +1,18 @@
+
+    INSERT INTO users (name, password, email) 
+    VALUES 
+('Jenifer Maxwell', 'password', 'jenifer_maxwell@goodmail.su'), 
+('Morgan Seagull', 'password', 'morgan_seagull@goodmail.em'), 
+('John Sikorski', 'password', 'john_sikorski@goodmail.em'), 
+('Anna Edison', 'password', 'anna_edison@darkside.org'), 
+('Jenifer Bear', 'password', 'jenifer_bear@sample.su'), 
+('Maria Maxwell', 'password', 'maria_maxwell@darkside.em'), 
+('Jeff Edison', 'password', 'jeff_edison@example.org'), 
+('Anna Racoon', 'password', 'anna_racoon@sample.su'), 
+('Olga White', 'password', 'olga_white@sample.ru'), 
+('Maria White', 'password', 'maria_white@example.org'), 
+('Morgan Einstein', 'password', 'morgan_einstein@goodmail.su'), 
+('Olga Einstein', 'password', 'olga_einstein@goodmail.su'), 
+('Jane Sikorski', 'password', 'jane_sikorski@goodmail.co'), 
+('Maria Seagull', 'password', 'maria_seagull@darkside.co'), 
+('Anna Maxwell', 'password', 'anna_maxwell@darkside.org');
