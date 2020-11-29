daba80e21491dfcaa7b10022ba482666a77c6cc7
Adam Thorne <ahthornev2@gmail.com>
2020-11-29 20:32:09 +0000

    Edit routes and helper files, move helper file to db
    Move the user-db-helpers file to db, follow same code style as Pavel and utilize the 'query' function he has created.

--- routes/users.js
+++ routes/users.js
@@ -7,27 +7,38 @@
 
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 module.exports = (db) => {
 
+  const login = (email, password) => {
+    //^^ log the user into the system with a given email/password, using getUserWithEmail to find the given user in the DB
+    // use bcrypt.compareSync to compare passwords, return user object upon successful validation
+
+    return db.findUserByEmail(email)
+      .then(user => {
+        if (bcrypt.compareSync(password, user.password)) {
+          return user;
+        }
+        return null;
+      });
+  };
+
   router.get('/login', (req, res) => {
     const { email, password } = req.body;
-    db.login(email, password)
+    login(email, password)
       .then(user => {
         if (!user) {
           res.error(401).render('error', { error: "Unauthorized" });
         }
-        req.session.user_id = user.id;
+        req.session.userid = user.id;
         res.redirect('/user/:id', user);
-      })
-      .catch((err) => {
-        console.error('Query error', err.stack);
       });
   });
   // ^^ more complicated user login, with given email/password, using hashed password and a redirection to the user/:id page.
 
   router.get('/login/:id', (req, res) => {
-    req.session.user_id = req.params.id;
+    req.session.userid = req.params.id;
     res.redirect('/');
   });
   // very simple user login, input ID and submit to login, set cookie to the ID of user
@@ -42,13 +53,16 @@ module.exports = (db) => {
 
 
   router.post('/register', (req, res) => {
+    // register a new user, recieve a user object from the request and pass it thru newUser helper function to add to database
+    // return with the new user row from database, set cookie to new user.id and redirect to newly created user/:id page
+
     const userObj = req.body;
-    db.addUser(userObj)
+    db.newUser(userObj)
       .then(user => {
         if (!user) {
           res.status(404).render('error', { error: "User not found!" });
         }
-        req.session.user_id = user.id;
+        req.session.userid = user.id;
         res.redirect('user', user);
       })
       .catch((err) => {
@@ -72,7 +86,7 @@ module.exports = (db) => {
 
     // when it is determined that active user is authorized to view page, use ID to return the user object from the DB
     // pass it into the res.render fn
-    db.getUserWithId(userID)
+    db.findUserByID(userID)
       .then(user => {
         if (!user) {
           res.status(404).render('error', { error: "No user with that ID!" });
@@ -81,29 +95,23 @@ module.exports = (db) => {
       });
   });
 
+
+  router.post('/user/:id', (req, res) => {
+    // check if active user is the user/:id in question by comparing to session.user_id
+
+    if (req.session.user_id !== req.params.id) {
+      res.statusCode(401).render('error', { error: "Unauthorized!"});
+    }
+
+    const { name, password, email } = req.body;
+    const userDetails = [name, password, email, req.session.user_id];
+
+    // update the user row within the database, and render user/:id with the updated information
+    db.editUser(userDetails)
+      .then(user => {
+        res.render('user', user);
+      });
+  });
+
   return router;
 };
-
-
-// return to this route later... editing user details...
-/*
-router.post('/user/:id', (req, res) => {
-  if (req.session.user_id !== req.params.id) {
-    res.statusCode(401).render('error');
-  }
-
-  const { name, password, email } = req.body;
-  const userDetails = [name, password, email, req.session.user_id];
-
-  db.query(`
-    UPDATE users
-    SET name = $1, password = $2, email = $3
-    WHERE id = $4
-    RETURNING *;
-    `, userDetails)
-    .then(data => {
-      const user = data.rows[0];
-      res.render('user', { user });
-    });
-});
-*/
