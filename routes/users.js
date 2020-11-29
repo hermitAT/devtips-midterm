9a0c39f833922e7167de9c5a2e4f3377f5729cbc
Adam Thorne <ahthornev2@gmail.com>
2020-11-29 02:33:24 +0000

    Add initial user routes
    Add initial user routes, very simple, just to login a user using their ID, logout and clear cookies, and a route to render the user/:id page.

--- routes/users.js
+++ routes/users.js
@@ -7,19 +7,64 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+  router.get('/login/:id', (req, res) => {
+    req.session.user_id = req.params.id;
+    res.redirect('/');
+  });
+  // very simple user login, input ID and submit to login, set cookie to the ID of user
+  // redirect to home page upon success
+
+
+  router.get('/logout', (req, res) => {
+    req.session = null;
+    res.redirect('/');
+  });
+  // clear cookies in session upon logout, redirect
+
+  router.get('/user/:id', (req, res) => {
+    if (!req.session.user_id) {
+      res.redirect('/');
+    }
+    // if no user logged in, redirect to home page
+
+    if (req.session.user_id !== )
+
+    const user_id = req.session.user_id;
+
+    db.query(`
+      SELECT * from users
+      WHERE id = $1;
+      `, [user_id])
       .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+        const user = data.rows[0];
+        res.render('user', { user });
       });
   });
+
+  /*
+  router.post('/user/:id', (req, res) => {
+    if (req.session.user_id !== req.params.id) {
+      res.statusCode(401).render('error');
+    }
+
+    const { name, password, email } = req.body;
+    const userDetails = [name, password, email, req.session.user_id];
+
+    db.query(`
+      UPDATE users
+      SET name = $1, password = $2, email = $3
+      WHERE id = $4
+      RETURNING *;
+      `, userDetails)
+      .then(data => {
+        const user = data.rows[0];
+        res.render('user', { user });
+      });
+  });
+  */
+
   return router;
 };
