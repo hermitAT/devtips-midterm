8288b78caa8fa8d7888da2264a08b4e2d7ed3f3b
Adam Thorne <ahthornev2@gmail.com>
2020-11-29 18:04:20 +0000

    Edit minor changes

--- routes/user_helpers.js
+++ routes/user_helpers.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 
+
 const findUserByEmail = (email) => {
+  // return a user object from the DB when a user with the given Email is found
 
   return db.query(`SELECT * FROM users WHERE email = $1;`, [email])
     .then(data => {
@@ -13,10 +15,12 @@ const findUserByEmail = (email) => {
       console.error('Query error', err.stack);
     });
 };
-// ^^ return a user object from the DB when a user with the given Email is found
+exports.findUserByEmail = findUserByEmail;
 
 
 const findUserByID = (id) => {
+  // return a user object from the DB for when a user with the given ID is found
+
   return db.query(`SELECT * FROM users WHERE id = $1;`, [id])
     .then(data => {
       if (!data.rows[0]) {
@@ -28,10 +32,12 @@ const findUserByID = (id) => {
       console.error('Query error', err.stack);
     });
 };
-// ^^ return a user object from the DB for when a user with the given ID is found
+exports.findUserByID = findUserByID;
 
 
 const newUser = (user) => {
+  // functionality to add a new user into the database, hashing the given password, and returning the new user object
+
   user.password = bcrypt.hashSync(user.password, 12);
   const values = [user.name, user.email, user.password];
 
@@ -46,10 +52,13 @@ const newUser = (user) => {
       console.error('Query error', err.stack);
     });
 };
-// ^^ functionality to add a new user into the database, hashing the given password, and returning the new user object
+exports.newUser = newUser;
 
 
 const login = (email, password) => {
+  //^^ log the user into the system with a given email/password, using getUserWithEmail to find the given user in the DB
+  // use bcrypt.compareSync to compare passwords, return user object upon successful validation
+
   return db.getUserWithEmail(email)
     .then(user => {
       if (bcrypt.compareSync(password, user.password)) {
@@ -58,12 +67,4 @@ const login = (email, password) => {
       return null;
     });
 };
-//^^ log the user into the system with a given email/password, using getUserWithEmail to find the given user in the DB
-// use bcrypt.compareSync to compare passwords, return user object upon successful validation
-
-module.exports = {
-  findUserByID,
-  findUserByEmail,
-  newUser,
-  login
-};
+exports.login = login;
