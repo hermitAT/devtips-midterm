2ceb1c430746d47478979eeb36576eba469801dd
Adam Thorne <ahthornev2@gmail.com>
2020-11-30 00:33:33 +0000

    Add cookie-session to server

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ const bodyParser = require("body-parser");
 const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
+const cookieSession = require('cookie-session');
 
 // PG database client/connection setup
 const { Pool } = require('pg');
 const dbParams = require('./lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
+
+// create cookie cookieSession
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
