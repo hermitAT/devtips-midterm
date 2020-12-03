eedec89add66d5160950ce35f8d40f02cc03531c
Jeremy Dombrowski <jeremy@meatify.com>
2020-12-03 06:45:19 -0800

    Compose front end js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,12 +4,35 @@ $(() => {
   autosize(document.querySelectorAll('textarea'));
 
   // The compose box
-  $('#compose-textarea-title, #compose-textarea-url, #compose-textarea-description').on('input propertychange', (e) => {
-    if (!$('#compose-textarea-title').val() && !$('#compose-textarea-url').val() && !$('#compose-textarea-description').val()) {
-      $('#compose-textarea-url').hide();
+  $('#compose-textarea-title, #compose-textarea-data, #compose-textarea-description, #compose-input-tags').on('input propertychange', (e) => {
+    if ($('#compose-textarea-title').val() || $('#compose-textarea-data').val() || $('#compose-textarea-description').val() || $('#compose-input-tags').val()) {
+      $('#compose-textarea-data').show();
+      if($('#compose-textarea-data').val() || $('#compose-textarea-description').val() || $('#compose-input-tags').val()){
+        $('#compose-metadata').show();
+      } else {
+        $('#compose-metadata').hide();
+      }
     } else {
-      $('#compose-textarea-url').show();
+      $('#compose-textarea-data').hide();
+      $('#compose-metadata').hide();
     }
+  });
+
+  // Compose box hidden radio select element via buttons
+
+
+  $('#form-compose-submit').on('click', function(event) {
+    event.preventDefault();
+    const form = $('#form-compose');
+    $.ajax({
+      type: form.attr('method'),
+      url: form.attr('action'),
+      data: form.serialize()
+    }).done(function(data) {
+      console.log('Success! ', data);
+    }).fail(function(err) {
+      console.log('Failure. ', err);
+    });
   });
 
 });
