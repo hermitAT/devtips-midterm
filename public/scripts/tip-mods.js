7ac66e05c1bb11bb03e7dbe6747c41a5a77bd754
Adam Thorne <ahthornev2@gmail.com>
2020-12-03 03:03:08 +0000

    Add scripts for POST and DELETE for likes/bookmarks

--- public/scripts/tip-mods.js
+++ public/scripts/tip-mods.js
@@ -1,11 +1,61 @@
-/*
+
 $(document).ready(function() {
 
 
-  $('.far').on('click', function(event) {
+  $('.far fa-thumbs-up').on('click', function(event) {
 
-    $
+    const $likeIcon = $(this);
+    const tip_id = $(this).closest("a").find('#tip_id').text();
 
+    $.ajax('/tip/:tip_id/like', {
+      method: 'POST',
+      data: { "tip_id": tip_id }
+    })
+      .done(function() {
+        $likeIcon.removeClass('far').addClass('fas');
+      });
   });
+
+  $('.fas fa-thumbs-up').on('click', function(event) {
+
+    const $likeIcon = $(this);
+    const tip_id = $(this).closest("a").find('#tip_id').text();
+
+    $.ajax('/tip/:tip_id/like', {
+      method: 'DELETE',
+      data: { "tip_id": tip_id }
+    })
+      .done(function() {
+        $likeIcon.removeClass('fas').addClass('far');
+      });
+  });
+
+  $('.far fa-bookmark').on('click', function(event) {
+
+    const $bookmarkIcon = $(this);
+    const tip_id = $(this).closest("a").find('#tip_id').text();
+
+    $.ajax('/tip/:tip_id/like', {
+      method: 'POST',
+      data: { "tip_id": tip_id }
+    })
+      .done(function() {
+        $bookmarkIcon.removeClass('far').addClass('fas');
+      });
+  });
+
+  $('.fas fa-bookmark').on('click', function(event) {
+
+    const $bookmarkIcon = $(this);
+    const tip_id = $(this).closest("a").find('#tip_id').text();
+
+    $.ajax('/tip/:tip_id/like', {
+      method: 'DELETE',
+      data: { "tip_id": tip_id }
+    })
+      .done(function() {
+        $bookmarkIcon.removeClass('fas').addClass('far');
+      });
+  });
+
 });
-*/
