55db9d1b2179c09a4612fde1186ce2156671cf6f
Jeremy Dombrowski <jeremy@meatify.com>
2020-12-03 05:47:18 -0800

    Compose box visibility script

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,15 @@
 
 $(() => {
   // Autoresize textarea elements
-  autosize(document.querySelector('textarea'));
+  autosize(document.querySelectorAll('textarea'));
+
+  // The compose box
+  $('#compose-textarea-title, #compose-textarea-url, #compose-textarea-description').on('input propertychange', (e) => {
+    if (!$('#compose-textarea-title').val() && !$('#compose-textarea-url').val() && !$('#compose-textarea-description').val()) {
+      $('#compose-textarea-url').hide();
+    } else {
+      $('#compose-textarea-url').show();
+    }
+  });
+
 });
